cd9ab2ebd701aabc91b351c754ee98e44f0030ff
thejeff77 <unspecified@unspecified.com>
2020-02-01 11:16:48 -0700

    Adding script and documentation

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# fastlane-meta-translate-mac
+# fastlane Metadata Translate for Mac/iOS
+
 Manage one set of metadata via fastlane, translate the rest
+
+Utilizes the translate cli https://github.com/soimort/translate-shell
+
+## Pre-requisites
+
+- install brew for mac
+- setup fastlane folder with downloaded metadata. Requires folders for all languages you have in app store connect within the metadata directory. Ex: metadata/es
+
+## Setup
+
+Put translate.sh in your fastlane folder, or add this repo as a submodule
+
+> git submodule add https://github.com/thejeff77/fastlane-meta-translate-mac.git
+
+Install dependent brew packages:
+
+> cd fastlane-meta-translate-mac
+> chmod 777 ./translate.sh
+> ./translate.sh --setup
+
+or:
+
+> ./translate.sh -s
+
+## Running translations:
+
+> ./translate.sh --searchpath metadata/ --file subtitle.txt --baselang en-US
+
+or the shorter version:
+
+> ./translate.sh -p metadata/ -f subtitle.txt -b en-US
+
+baselang must be an existing folder with metadata. The example command above will use the metadata/en-US/subtitle.txt file as the base for translating subtitle.txt into the other language directories that exist in the fastlane/metadata folder.
+
+Options:
+
+- (-s|--setup) Install required packages for the script to run.
+- (-p|--searchpath) The relative path to the fastlane metadata folder to use for translations.
+- (-f|--file) The file to translate. Eg: description.text, release_notes.txt
+- (-b|--baselang) The base language folder to use as the source of the translation.
+
+NOTES:
+- Does not support locale. If you have a zh-Hans folder, the translator will run translation for zh. The reason for this is due to the limited locale support for the translate cli (see documentation here: https://github.com/soimort/translate-shell
---
+++ translate.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -s|--setup)
+    INIT=YES
+    shift # past argument
+    ;;
+    -p|--searchpath)
+    SEARCHPATH="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -f|--file)
+    FILE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -b|--baselang)
+    BASELANG="$2"
+    shift # past argument
+    shift # past value
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo -e "\n Command Opts:\n"
+
+echo "INIT  = ${INIT}"
+echo "SEARCH PATH     = ${SEARCHPATH}"
+echo "TRANSLATE FILE    = ${FILE}"
+echo "BASELANG         = ${BASELANG}"
+
+#echo "Number files in SEARCH PATH with EXTENSION:" $(ls -1 "${SEARCHPATH}"/*."${EXTENSION}" | wc -l)
+#if [[ -n $1 ]]; then
+#    echo "Last line of file specified as non-opt/last argument:"
+#    tail -1 "$1"
+#fi
+
+if [[ $INIT == *"YES"* ]]; then
+  # Based on https://github.com/soimort/translate-shell setup instructions
+  echo -e "\nInstalling required dependencies\n"
+
+  # Install fribidi
+  brew install fribidi
+
+  # Install curl
+  brew install curl
+
+  # Install gawk v4
+  brew install gawk
+
+  # translate-shell
+  brew install translate-shell
+fi
+
+if [ -n "$SEARCHPATH" ]; then
+  echo -e "\nChecking ${SEARCHPATH} for language directories\n"
+  files=($(ls $SEARCHPATH))
+  for item in ${files[*]}
+    do
+      size=${#item} 
+      if [[ $item == *"-"* ]] && [[ $item != *".txt"* ]]; then
+        LANGDIRS+=( $item )
+	printf "   %s\n" $item
+      elif [[ $size == 2 ]] && [[ $item != *".txt"* ]]; then
+        LANGDIRS+=( $item )
+        printf "   %s\n" $item
+      fi
+    done
+  len=${#LANGDIRS[@]}
+  if [[ $len == 0 ]]; then
+    echo -e "\nERROR - Did not find any language directories in searchpath use metadata/ to your fastlane metadata path\n"
+    exit 0
+  fi
+else
+  echo -e "\nARGV ERROR - No search path set, exiting."
+  exit 0
+fi
+
+
+if [ -n "$FILE" ] && [ -n "$BASELANG" ] && [ -n "$SEARCHPATH" ]; then
+  BASEFILEPATH="${SEARCHPATH}${BASELANG}/$FILE"
+  # Ex: en-US to en
+  BASELANGONLY=${BASELANG:0:2}
+
+  if [ ! -f "$BASEFILEPATH" ]; then
+    echo -e "\nERROR - $FILE does not exist in $BASEFILEPATH, specify an existing filename and valid baselang\n"
+    exit 0
+  fi
+
+  for langdir in ${LANGDIRS[*]}
+    do
+      if [ "$langdir" != "$BASELANG" ]; then
+        echo "translating $langdir"
+        DESTBASELANGONLY=${langdir:0:2}
+        DESTFILEPATH="${SEARCHPATH}${langdir}/$FILE"
+        rm $DESTFILEPATH > /dev/null 2>&1
+        TRANSFILEPATH="file://${BASEFILEPATH}"
+        echo "$BASELANGONLY:$DESTBASELANGONLY $TRANSFILEPATH > $DESTFILEPATH"
+        trans $BASELANGONLY:$DESTBASELANGONLY $TRANSFILEPATH > $DESTFILEPATH
+        echo -e "\nTranslated $FILE to $langdir"
+      fi
+    done
+
+else
+  echo -e "\nARGV ERROR - All of file, searchpath, and baselang args are required to start translation. Exiting."
+  exit 0
+fi
